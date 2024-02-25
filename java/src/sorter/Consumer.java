7309dd1619c94a38cfce0dced37cc70b8faef9f6
morgan <mmnelson7@wisc.edu>
2024-02-25 10:07:06 -0600

    starting to add comments to java tests

--- java/src/sorter/Consumer.java
+++ java/src/sorter/Consumer.java
@@ -1,9 +1,12 @@
 package sorter;
 
+/**
+ * This class is utilized to receive records using the Provider class
+ * Continues to obtain new records until a null record is returned
+ */
 public class Consumer {
 
     public final Provider source;
-    
     public Consumer(Provider source) {
         this.source = source;
     }
--- java/src/sorter/Provider.java
+++ java/src/sorter/Provider.java
@@ -1,5 +1,9 @@
 package sorter;
 
+/**
+ * This interface is utilized to generate records
+ */
 public interface Provider {
+    // returns the next record or null if all desired records have been retrieved
     Record next();
 }
