ea2f7900fe95d16381cee577b5284e9cf7f07a63
morgan <mmnelson7@wisc.edu>
2024-02-25 12:55:10 -0600

    added explanation comments to the last two java files

--- java/src/sorter/TreeSorter.java
+++ java/src/sorter/TreeSorter.java
@@ -3,32 +3,50 @@ package sorter;
 import java.util.Iterator;
 import java.util.TreeMap;
 
+/**
+ * This class is used retrieve a sorted TreeMap of records and compare record's key/indexes within the TreeMap
+ */
+
 public class TreeSorter implements Provider{
-  
+
+    // stores all records in sorted order
     private Iterator<Record> records;
     
     static class Key implements Comparable<Key>{
+        // key of the record
         private final long key;
+        // record's index in Iterator
         private final long index;
 
         public Key(long key, long index) {
             this.key = key;
             this.index = index;
         }
-        
+
+        /**
+         * This method is utilized to compare a record to another record based on its key or index
+         * @param other the other Key to compare to the current Key
+         * @return -1 if current key/index is less than other key/index or 1 if current key/index is greater than other key/index
+         */
         @Override
         public int compareTo(Key other) {
             if(this.key < other.key) return -1;
             if(this.key > other.key) return +1;
+            // if key's are the same, then compares indexes
             if(this.index < other.index) return -1;
             if(this.index > other.index) return +1;
             return 0;
         }
     }
-    
+
+    /**
+     * Creates a tree map of records in sorted order that can be iterated through
+     * @param source the Provider to create the records
+     */
     public TreeSorter(Provider source) {
         long index = 0;
         TreeMap<Key,Record> sorted = new TreeMap<>();
+        // adds all record to TreeMap until count has been reached
         while(true) {
             Record record = source.next();
             if(record == null) break;
@@ -36,7 +54,11 @@ public class TreeSorter implements Provider{
         }
         records = sorted.values().iterator();
     }
-    
+
+    /**
+     * Returns the next sorted record or null if no other records are in list
+     * @return next record or null
+     */
     @Override
     public Record next() {
         if(records.hasNext()) return records.next();
--- java/src/sorter/Witness.java
+++ java/src/sorter/Witness.java
@@ -1,9 +1,13 @@
 package sorter;
 
+/**
+ * This class will be utilized to verify the correctness of the records sorting algorithm
+ */
 public class Witness implements Provider {
     
     public final Provider source;
-    
+
+    // keeps track of all the records returned by next()
     private long count = 0;
     private long crc = 0;
     private Long lastKey = null;
@@ -13,11 +17,15 @@ public class Witness implements Provider {
         this.source = source;
     }
 
+    /**
+     * Returns the next record in the list and checks that each record key is sorted
+     * @return next record or null if no more records exists
+     */
     @Override
     public Record next() {
         Record result = source.next();
         if(result == null) return null;
-        
+        // checks that keys are sorted; sets isSorted to false if not
         if(isSorted) {
             long key = result.getKey();
             if(lastKey!=null) {
@@ -25,20 +33,22 @@ public class Witness implements Provider {
             }
             lastKey = key;
         }
-        
         count++;
+        // computes checksum value of record
         crc ^= result.getCrc();
         return result;
     }
 
+    // returns the number of records returned by next()
     public long getCount() {
         return count;
     }
 
+    // returns the checksum value
     public long getCrc() {
         return crc;
     }
-
+    // returns whether the record key's/indexes have been sorted up to this point
     public boolean isSorted() {
         return isSorted;
     }
