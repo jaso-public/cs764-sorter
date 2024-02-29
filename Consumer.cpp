8106fcc37bb3aef4a9e8ec92c3dab402d0cd0321
morgan <mmnelson7@wisc.edu>
2024-02-29 13:57:49 -0600

    ending on consumer

---
+++ Consumer.cpp
@@ -0,0 +1,13 @@
+#include "Consumer.h"
+#include "Provider.h"
+#include "Record.h"
+
+//TODO: find out why this is not working
+Consumer::Consumer(Provider givenSource) {
+    source = givenSource;
+}
+
+void Consumer::consume() {
+
+}
+
