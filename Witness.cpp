d89f1ef31c04d37c813213fed2fe2c4decf7c445
morgan <mmnelson7@wisc.edu>
2024-03-03 09:41:20 -0600

    outlined witness class file

--- Witness.cpp
+++ Witness.cpp
@@ -1,5 +1,45 @@
-//
-// Created by Morgan Nelson on 3/3/24.
-//
-
 #include "Witness.h"
+#include "Provider.h"
+#include "Record.h"
+using namespace std;
+#include <iostream>
+
+/**
+ * Initializes witness constructor
+ * @param givenSource the source to get records from
+ */
+Witness::Witness(Provider givenSource) {
+    source = givenSource;
+}
+
+/**
+* Returns a pointer to the next record in the list and checks that each record key is sorted
+* @return pointer to next record or null if no more records exists
+*/
+Record *Witness::next() {
+
+}
+
+/**
+  * Gets total number of generated records
+  * @return the total number of record returned by next()
+  */
+long Witness::getCount() {
+
+}
+
+/**
+  * Returns the checksum value of all the records
+  * @return checksum value
+  */
+long Witness::getCrc() {
+
+}
+
+/**
+ * Returns a true or false value indicating whether the records are sorted or not
+ * @return true if records are sorted else false
+ */
+bool Witness::checkSorted() {
+
+}
