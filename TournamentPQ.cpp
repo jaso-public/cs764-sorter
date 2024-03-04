3eab0b3d5292575e28aaa6254426e8615d4089d2
morgan <mmnelson7@wisc.edu>
2024-03-04 13:09:36 -0600

    outlined TournamentPQ.h

---
+++ TournamentPQ.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Morgan Nelson on 3/4/24.
+//
+
+#include "TournamentPQ.h"
---
+++ TournamentPQ.h
@@ -0,0 +1,44 @@
+#ifndef CS764_SORTER_TOURNAMENTPQ_H
+#define CS764_SORTER_TOURNAMENTPQ_H
+#include "Provider.h"
+
+
+/**
+ * This is the tournament priority queue that will be utilized for sorting
+ */
+class TournamentPQ:Provider {
+private:
+    // an array of providers to generate records
+    Provider providers[] final;
+    // array of created records
+    Record records[] final;
+    // array of loser keys
+    uint64_t losers[] final;
+    // the number of record providers
+    int size final;
+    /**
+    * Runs a match between to provider
+    *
+    * @param first the first provider in the match
+    * @param second the second provider in the match
+    * @return true if the first provider wins the match, false otherwise.
+    */
+    bool firstWin(int first, int second);
+
+public:
+    /**
+     * TournamentPQ constructor
+     * Determines which records are winners and losers in the given tournament
+     * @param givenProviders an array of providers that can generate records
+     */
+    TournamentPQ(Provider givenProviders[]);
+    /**
+     * Returns the next winning record of the tournament
+     * @return the next winning record
+     */
+    Record next();
+
+};
+
+
+#endif //CS764_SORTER_TOURNAMENTPQ_H
