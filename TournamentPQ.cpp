9fd679925cb96685ef2708945b0268a8a04411b0
morgan <mmnelson7@wisc.edu>
2024-03-04 13:36:22 -0600

    started implementing TournamentPQ.cpp methods

--- TournamentPQ.cpp
+++ TournamentPQ.cpp
@@ -1,5 +1,54 @@
-//
-// Created by Morgan Nelson on 3/4/24.
-//
-
 #include "TournamentPQ.h"
+#include "Provider.h"
+#include "Record.h"
+#include<iostream>
+#include<array>
+
+/**
+   * Initializes TournamentPQ constructor
+   * Determines which records are winners and losers in the given tournament
+   * @param givenProviders an array of providers that can generate records
+   */
+TournamentPQ::TournamentPQ(Provider *givenProviders, uint32_t givenKeyOffset) {
+    // sets given providers to class providers variable
+    providers = *givenProviders;
+    // sets givenKeyOffset to class offset variable
+    keyOffset = givenKeyOffset;
+
+    // calculates the total number of given providers
+    size = sizeof(providers)/sizeof(providers[0]);
+
+    // creates fills the records array with the next value from every provider
+    this->records = new Record(size, keyOffset);
+    for (int i = 0; i< size; i++){
+        records[i] = providers[i].next();
+    }
+
+    // creates losers array to be the size of the number of next records obtained/number of providers
+    loser = malloc(sizeof(size));
+    // creates a winners array of keys
+    uint64_t* winnersArray = malloc(sizeof(size));
+    uint64_t *winners = (uint64_t*) winnersArray;
+
+    //TODO: begin conceptual explanation here
+    for (int match=size-1; match > 0; match--){
+        int h1 = match * 2;
+        int h2 = h1 + 1;
+        if (h1>=size) h1 = h1-size; else h1 = winners[h1];
+        if (h2>=size) h2 = h2-size; else h2 = winners[h2];
+
+        if (firstWin(h1,h2)){
+            winners[match] = h1;
+            loser[match] = h2;
+        } else {
+            winners[match] = h2;
+            losers[match] = h1;
+        }
+
+        // loser[0] is really the winner of the overall tournament
+        losers[0] = winners[1];
+    }
+}
+
+//TODO: ending on next record method
+// need to make it return a pointer instead of a record
--- TournamentPQ.h
+++ TournamentPQ.h
@@ -9,13 +9,15 @@
 class TournamentPQ:Provider {
 private:
     // an array of providers to generate records
-    Provider providers[] final;
+    Provider* providers final;
     // array of created records
-    Record records[] final;
+    Record* records final;
     // array of loser keys
-    uint64_t losers[] final;
+    uint64_t* losers final;
     // the number of record providers
-    int size final;
+    uint64_t size final;
+    // key offset of record
+    uint32_t keyOffset;
     /**
     * Runs a match between to provider
     *
@@ -30,8 +32,9 @@ public:
      * TournamentPQ constructor
      * Determines which records are winners and losers in the given tournament
      * @param givenProviders an array of providers that can generate records
+     * @param keyOffset the key offset value of the record
      */
-    TournamentPQ(Provider givenProviders[]);
+    TournamentPQ(Provider *givenProviders, uint32_t givenKeyOffset);
     /**
      * Returns the next winning record of the tournament
      * @return the next winning record
