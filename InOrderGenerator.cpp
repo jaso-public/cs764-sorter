14bfe9d5f16824f1f6499003fed8c660e6be6299
morgan <mmnelson7@wisc.edu>
2024-03-07 08:36:47 -0600

    InOrderGenerator.h complete

---
+++ InOrderGenerator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Morgan Nelson on 3/7/24.
+//
+
+#include "InOrderGenerator.h"
---
+++ InOrderGenerator.h
@@ -0,0 +1,27 @@
+#ifndef CS764_SORTER_INORDERGENERATOR_H
+#define CS764_SORTER_INORDERGENERATOR_H
+#include "Provider.h";
+#include <stdlib.h>
+
+/**
+ * This class can be used to generate records of a certain size and amount
+ */
+class InOrderGenerator:Provider {
+public:
+    // represents total number of records to generate
+    long count;
+    // represents the size of the records
+    int size;
+    // class constructor
+    InOrderGenerator(long givenCount, int givenSize);\
+    // returns a pointer to the next record or a null pointer if a next record does not exist
+    Record* next();
+private:
+    // stores actual number of records currently generated
+    long generate = 0;
+    // random number
+    int rng = rand();
+};
+
+
+#endif //CS764_SORTER_INORDERGENERATOR_H
