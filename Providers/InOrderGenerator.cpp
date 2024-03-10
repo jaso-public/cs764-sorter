f8a3e4cf6ec1f9a95dd54f95725c1a533e21ddd8
morgan <mmnelson7@wisc.edu>
2024-03-10 12:33:15 -0500

    in order generator complete

--- Providers/InOrderGenerator.cpp
+++ Providers/InOrderGenerator.cpp
@@ -11,9 +11,13 @@ InOrderGenerator::InOrderGenerator(long givenCount, int givenSize) {
     * Continues to generate a new record in memory until records of the desired count have been generated
     * @return a pointer to the next record or a null pointer if count has been reached
     */
-Record *InOrderGenerator::next() {
+Record* InOrderGenerator::next() {
     // returns a null pointer when no more records are to be generated
     if (generated >= count) return nullptr;
-    //TODO: ending here
+    // returns a newly created record in memory
+    Record r(size, 8);
+    generated++;
+    Record* ptr = &r;
+    return ptr;
 }
 
