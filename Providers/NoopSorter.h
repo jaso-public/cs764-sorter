fd8dcf0e99fc85081b63bdc522748efc9f3544bc
morgan <mmnelson7@wisc.edu>
2024-03-10 13:38:54 -0500

    moved working on files to not done folder

---
+++ Providers/NoopSorter.h
@@ -0,0 +1,14 @@
+//
+// Created by Morgan Nelson on 3/10/24.
+//
+
+#ifndef CS764_SORTER_NOOPSORTER_H
+#define CS764_SORTER_NOOPSORTER_H
+
+
+class NoopSorter {
+
+};
+
+
+#endif //CS764_SORTER_NOOPSORTER_H
