f6cafcd784ebb6c66dd069cd10b60f8a8e4b8293
morgan <mmnelson7@wisc.edu>
2024-03-10 13:43:08 -0500

    Noop sorter done

--- Providers/NoopSorter.cpp
+++ Providers/NoopSorter.cpp
@@ -1,2 +1,13 @@
 #include "NoopSorter.h"
+/**
+  * Class constructor that will set the provider
+  * @param givenProvider the given provider
+  */
+NoopSorter::NoopSorter(Provider givenProvider) {
+    this->source = givenProvider;
+}
 
+// returns the provider's next generated record pointer
+Record* NoopSorter::next() {
+    return source.next();
+}
