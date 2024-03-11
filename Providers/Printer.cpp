0713a88fa25bbdf0ac8d07beca8cb0ef8d2f220b
morgan <mmnelson7@wisc.edu>
2024-03-11 14:56:03 -0500

    Printer done

--- Providers/Printer.cpp
+++ Providers/Printer.cpp
@@ -1,5 +1,6 @@
 #include "Printer.h"
 #include <iostream>
+#include <sys/time.h>
 using namespace std;
 /**
    * Class constructor of the printer that sets the given variables to the class variables
@@ -16,8 +17,21 @@ Printer::Printer(Provider givenSource, string givenMessage) {
  * If the record is null, it will print out the time elapsed (milliseconds), message, and a null response to console
  * If the record is not null, it will print out the time elapsed (milliseconds), message, record's key, and checksum to console
  * @returns a pointer to the record or a null pointer
+ * Sources for time:
+ * https://www.tutorialspoint.com/cplusplus/cpp_date_time.htm
+ * https://codereview.stackexchange.com/questions/11921/getting-current-time-with-milliseconds
  */
 Record* Printer::next() {
+    // obtains the current time to the millisecond to print out
+    time_t now = time(0);
+    tm *localTime = localtime(&now);
+    timeval currentTime;
+    gettimeofday(&currentTime, NULL);
+    int milliSeconds = currentTime.tv_usec / 1000;
+    cout << "Current Time: "<< localTime->tm_hour << ":";
+    cout << localTime->tm_min << ":";
+    cout << localTime->tm_sec << ":";
+    cout << milliSeconds;
     Record* recordPtr = source.next();
     // if record pointer is null
     if (!recordPtr){
