813b38b2d4ceb8ccfd25af81104d3a7250cf5a78
morgan <mmnelson7@wisc.edu>
2024-03-17 06:55:50 -0500

    made SorterConfig.cpp

--- SorterConfig.cpp
+++ SorterConfig.cpp
@@ -1,5 +1,8 @@
 #include "SorterConfig.h"
 
+/**
+ * Class constructor that will initialize the IODevices with the right files
+ */
 SorterConfig::SorterConfig()
     : hddDevice("hddStaging.tmp"), ssdDevice("ssdStaging.tmp")
 {
