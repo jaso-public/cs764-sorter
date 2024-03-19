27e60934f85e7f91dd69ea98e9642ad6af91b9e8
morgan <mmnelson7@wisc.edu>
2024-03-18 20:15:24 -0500

    starting Sorter file

--- Sorter.cpp
+++ Sorter.cpp
@@ -1,1 +1,32 @@
 #include "Sorter.h"
+#include "Providers/SingleProvider.h"
+
+Sorter::Sorter(SorterConfig cfg, Provider source, int recordSize) {
+    this->cfg = cfg;
+    this->source = source;
+    this->recordSize = recordSize;
+
+
+    ssdRemaining = cfg.ssdStorageSize;
+
+    this->buffer = new char[cfg.memoryBlockSize * cfg.memoryBlockCount];
+    lastMemoryRun = sizeof(buffer);
+
+    this->sortedProvider = startSort();
+}
+
+Record* Sorter::next() {
+    return sortedProvider.next();
+}
+
+//TODO: here
+Provider Sorter::startSort() {
+    Record.resetCompareCount();
+
+    int maxRecordsPerRun = cfg.memoryBlockSize / recordSize;
+
+    SingleProvider s();
+    vector<SingleProvider> singles();
+
+
+}
