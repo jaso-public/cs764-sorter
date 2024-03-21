75a9cda77fff1ddd7b2a9a99a4a81ef86b023f1a
morgan <mmnelson7@wisc.edu>
2024-03-21 05:35:53 -0500

    building CrcRandomGenerator.cpp

--- Testing/CrcRandomGenerator.h
+++ Testing/CrcRandomGenerator.h
@@ -1,9 +1,19 @@
 #ifndef CS764_SORTER_CRCRANDOMGENERATOR_H
 #define CS764_SORTER_CRCRANDOMGENERATOR_H
+#include <stdlib.h>
+#include "../Records/Record.h"
 
 
 class CrcRandomGenerator {
-
+public:
+    long count;
+    int size;
+    CrcRandomGenerator(long count, int size);
+    Record* next();
+    void verifyCrc(Record rec);
+private:
+    long generated = 0;
+    int rng = rand();
 };
 
 
