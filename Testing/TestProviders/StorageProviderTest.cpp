39dd54ace87e3d5b2e3620b01850bb7be75b0b96
morgan <mmnelson7@wisc.edu>
2024-03-21 21:47:43 -0500

    ending on StorageProviderTest.cpp

--- Testing/TestProviders/StorageProviderTest.cpp
+++ Testing/TestProviders/StorageProviderTest.cpp
@@ -1,5 +1,48 @@
-//
-// Created by Morgan Nelson on 3/21/24.
-//
-
 #include "StorageProviderTest.h"
+#include "RandomGenerator.h"
+#include "Witness.h"
+
+void StorageProviderTest::doTest(int recordSize, long recordCount, int stagingLength, int bufferLength) {
+
+    RandomGenerator rg(recordCount, recordSize);
+    Witness before(rg);
+
+    //TODO: here
+    File storageFile = File.createTempFile("storage", ".tmp");
+    storageFile.deleteOnExit();
+    IoDevice storage = new IoDevice(storageFile);
+
+    long storageOffset = 0;
+    while(true) {
+        Record rec = before.next();
+        if(rec==null) break;
+        storage.write(storageOffset, rec.data, 0, rec.data.length);
+        storageOffset += rec.data.length;
+    }
+
+    byte[] memory = new byte[10*1024*1024]; // 10MB
+
+
+    long storageStartOffset = 0; // we wrote the records at offset zero
+
+    byte[] buffer = memory;
+    int bufferStartOffset = 20;
+
+
+    StorageProvider sp = new StorageProvider(
+            recordSize,
+            recordCount,
+            storage,
+            storageStartOffset,
+            buffer,
+            bufferStartOffset,
+            bufferLength);
+
+
+    Witness after = new Witness(sp);
+    Consumer c = new Consumer(after);
+    c.consume();
+
+    assertEquals(before.getCount(), after.getCount());
+    assertEquals(before.getCrc(), after.getCrc());
+}
