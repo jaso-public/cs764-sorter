b363f9ca3578197ec1221454a5e05e9514547f6b
morgan <mmnelson7@wisc.edu>
2024-03-22 09:57:05 -0500

    SpillOneBlockToSsd.cpp complete

--- Testing/SpillOneBlockToSsd.cpp
+++ Testing/SpillOneBlockToSsd.cpp
@@ -1,1 +1,35 @@
 #include "SpillOneBlockToSsd.h"
+#include "./TestProviders/RandomGenerator.h"
+#include "../Witness.h"
+#include "../Config/SorterConfig.h"
+#include "../Sort/Sorter.h"
+#include "../Consumer.h"
+#include <cassert>
+#include <string>
+using namespace std;
+
+
+void SpillOneBlockToSsd::testSpillToSsdFewBlocks() {
+    int recordSize = 1024;
+    int recordCount = 1024*100;
+    int keyOffset = 8;
+
+    string test = "testSpillToSsdFewBlocks: ";
+
+    RandomGenerator rng(recordCount, recordSize);
+    Provider generator(rng);
+    Witness lower(generator);
+    SorterConfig cgf;
+    Sorter sorter(cgf, lower, recordSize, keyOffset);
+    Witness upper(sorter);
+    Consumer consumer(upper);
+    consumer.consume();
+
+    sorter.printStats();
+
+    assert(("The count of the lower witness should have equaled the record count", recordCount == lower.getCount()));
+    assert(("The count of the upper witness should equaled the record count", recordCount == upper.getCount()));
+    assert(("The checksum of the lower witness did not equal the checksum of the upper but should have", lower.getCrc() == upper.getCrc()));
+    assert(("The lower was sorted but should not have been", !lower.isSorted));
+    assert(("The upper witness was not sorted but should have been", upper.isSorted));
+}
