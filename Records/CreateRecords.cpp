aacc12a3eee54d51c4171a7df723534b1b235ab3
morgan <mmnelson7@wisc.edu>
2024-03-25 11:19:38 -0500

    making an input file of records

---
+++ Records/CreateRecords.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Morgan Nelson on 3/25/24.
+//
+
+#include "CreateRecords.h"
--- Records/Record.cpp
+++ Records/Record.cpp
@@ -2,7 +2,6 @@
 #include "Record.h"
 #include <stdlib.h>
 #include <string.h>
-#include <iostream>
 using namespace std;
 
 /**
