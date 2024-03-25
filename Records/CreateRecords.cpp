b12e16f75f8f88a23e0b0b7a413168d74f28a690
morgan <mmnelson7@wisc.edu>
2024-03-25 11:40:20 -0500

    trying to make create records compile

--- Records/CreateRecords.cpp
+++ Records/CreateRecords.cpp
@@ -1,5 +1,47 @@
-//
-// Created by Morgan Nelson on 3/25/24.
-//
-
 #include "CreateRecords.h"
+
+CreateRecords::CreateRecords(int recordSize, int numberOfRecords) {
+    this->recordSize = recordSize;
+    this->numberOfRecords = numberOfRecords;
+    this->generated = 0;
+    this->data = 10;
+    write();
+}
+
+void CreateRecords::write() {
+    // Open file stream for writing
+    ofstream outFile("input.txt", std::ios::binary);
+    if (!outFile) {
+        std::cerr << "Failed to open file for writing!" << std::endl;
+    }
+    while (generated < this->numberOfRecords){
+        Record r(10, 8);
+        outFile << r.record << std::endl;
+        generated++;
+    }
+    // Close file stream
+    outFile.close();
+}
+
+void CreateRecords::read() {
+    std::ifstream inFile("input.txt", std::ios::binary);
+    if (!inFile) {
+        cerr << "Failed to open file for reading!" << std::endl;
+    }
+    // Read and output every piece of data in the file
+    std::string data;
+    while (inFile >> data) { // Read data until end of file
+        cout << data << std::endl; // Output data
+    }
+    // Close file stream
+    inFile.close();
+}
+
+
+int main() {
+    // Read object from file
+    CreateRecords newObj(10, 10);
+    newObj.read();
+
+    return 0;
+}
--- Records/CreateRecords.h
+++ Records/CreateRecords.h
@@ -2,7 +2,7 @@
 #define CS764_SORTER_CREATERECORDS_H
 #include <iostream>
 #include <fstream>
-#include "./Record.h"
+#include "Record.h"
 using namespace std;
 
 /**
@@ -11,64 +11,17 @@ using namespace std;
 class CreateRecords {
 
 public:
-    Record record;
     int recordSize;
     int numberOfRecords;
     int generated;
+    int data;
 
-    CreateRecords(int recordSize, int numberOfRecords){
-        this->recordSize = recordSize;
-        this->numberOfRecords = numberOfRecords;
-        this->generated = 0;
-        write();
-    }
+    CreateRecords(int recordSize, int numberOfRecords);
 
-    void write(){
-        // Open file stream for writing
-        ofstream outFile("input.txt", std::ios::binary);
-        if (!outFile) {
-            std::cerr << "Failed to open file for writing!" << std::endl;
-        }
-        while (generated < numberOfRecords){
-            Record r(10, 8);
-            outFile << r.record << std::endl;
-            generated++;
-        }
-        // Close file stream
-        outFile.close();
-    }
+    void write();
 
-    void read(istream & is){
-        // Read and output every piece of data in the file
-        std::string data;
-        while (is >> data) { // Read data until end of file
-            std::cout << data << std::endl; // Output data
-        }
-
-       // is.read(reinterpret_cast<char*>(&data), sizeof(data));
-    }
+    void read();
 };
-
-int main() {
-    // Open file stream for reading
-    std::ifstream inFile("input.txt", std::ios::binary);
-    if (!inFile) {
-        std::cerr << "Failed to open file for reading!" << std::endl;
-        return 1;
-    }
-
-    // Read object from file
-    CreateRecords newObj(10, 10);
-    newObj.read(inFile);
-
-    // Close file stream
-    inFile.close();
-
-    // Output the deserialized object
-    std::cout << "Deserialized object: " << record. << std::endl;
-
-    return 0;
-}
 
 
 #endif //CS764_SORTER_CREATERECORDS_H
