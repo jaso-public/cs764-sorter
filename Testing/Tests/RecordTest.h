f7c12cbcf722e502da01c45862e0c040d0c73d1b
morgan <mmnelson7@wisc.edu>
2024-03-26 10:24:54 -0500

    outlined record tests

---
+++ Testing/Tests/RecordTest.h
@@ -0,0 +1,14 @@
+#ifndef CS764_SORTER_RECORDTEST_H
+#define CS764_SORTER_RECORDTEST_H
+
+
+class RecordTest {
+public:
+    void testGetRecordKey();
+    void testCheckSum();
+    void testCompare();
+    void testStoring();
+};
+
+
+#endif //CS764_SORTER_RECORDTEST_H
