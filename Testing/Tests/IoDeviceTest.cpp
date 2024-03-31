f298fc0954f7b3fb4bb5dd33305f005b856bc96b
morgan <mmnelson7@wisc.edu>
2024-03-31 16:24:36 -0500

    Outlined IoDeviceTest.cpp

---
+++ Testing/Tests/IoDeviceTest.cpp
@@ -0,0 +1,15 @@
+#include "IoDeviceTest.h"
+
+void IoDeviceTest::testRead() {
+
+}
+
+void IoDeviceTest::testWrite() {
+
+}
+
+int main(){
+    IoDeviceTest test;
+    test.testRead();
+    test.testWrite();
+}
---
+++ Testing/Tests/IoDeviceTest.h
@@ -0,0 +1,12 @@
+#ifndef CS764_SORTER_IODEVICETEST_H
+#define CS764_SORTER_IODEVICETEST_H
+
+
+class IoDeviceTest {
+public:
+    void testRead();
+    void testWrite();
+};
+
+
+#endif //CS764_SORTER_IODEVICETEST_H
