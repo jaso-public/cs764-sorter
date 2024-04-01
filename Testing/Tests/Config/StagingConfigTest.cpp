f14073377b3853673c4d6640e6130fdc4bec8424
morgan <mmnelson7@wisc.edu>
2024-03-31 21:03:47 -0500

    made StagingConfigTest.h

---
+++ Testing/Tests/Config/StagingConfigTest.cpp
@@ -0,0 +1,10 @@
+#include "StagingConfigTest.h"
+
+void StagingConfigTest::testInitialization() {
+    StagingConfig config;
+}
+
+int main(){
+    StagingConfigTest test;
+    test.testInitialization();
+}
---
+++ Testing/Tests/Config/StagingConfigTest.h
@@ -0,0 +1,13 @@
+#ifndef CS764_SORTER_STAGINGCONFIGTEST_H
+#define CS764_SORTER_STAGINGCONFIGTEST_H
+#include "Config/StagingConfig.h"
+#include "assert.h"
+
+
+class StagingConfigTest {
+public:
+    void testInitialization();
+};
+
+
+#endif //CS764_SORTER_STAGINGCONFIGTEST_H
