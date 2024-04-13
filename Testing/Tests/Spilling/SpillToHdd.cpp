e7d4b2b79be60584595a10714d46d0e1c723d038
morgan <mmnelson7@wisc.edu>
2024-04-13 13:48:21 -0500

    same as before

--- Testing/Tests/Spilling/SpillToHdd.cpp
+++ Testing/Tests/Spilling/SpillToHdd.cpp
@@ -8,9 +8,20 @@ using namespace std;
 
 void testSpillToHdd() {
     string test = "testSpillToHdd: ";
-    unique_ptr<SorterConfig> cfg = make_unique<SorterConfig>();
+    Record::staticInitialize(100);
+    int recordCount = 1;
 
-    auto source = make_shared<RandomProvider>(10);
+    unique_ptr<SorterConfig> cfg = make_unique<SorterConfig>();
+    cfg->memoryBlockCount = 10;
+    cfg->memoryBlockSize = 1000;
+    auto ssdDevice = make_shared<IODevice>("ssd.stage");
+    auto hddDevice = make_shared<IODevice>("hdd.stage");
+//    cfg->ssdDevice = ssdDevice;
+//    cfg->hddDevice = hddDevice;
+
+
+
+    auto source = make_shared<RandomProvider>(recordCount);
     auto lower = make_shared<Witness>(source);
     auto sorter = make_shared<Sorter>(cfg, lower);
     auto upper = make_shared<Witness>(sorter);
@@ -18,13 +29,20 @@ void testSpillToHdd() {
 
 
     while(true) {
-        shared_ptr<Record> recordPtr = printer->next();
-        if(recordPtr) break;
-        isCrcValid(recordPtr);
+        shared_ptr<Record> recordPtr = upper->next();
+        if(recordPtr == nullptr) break;
+//        isCrcValid(recordPtr);
     }
-    sorter->writeStats(std::cout);
-    assert(("The count of the lower witness should have equaled the record count" && 10 == lower->getCount()));
-    assert(("The count of the upper witness should equaled the record count" && 10  == upper->getCount()));
+    std::ostream* out = &std::cout;
+
+    sorter->writeStats(*out);
+    lower->writeStats(*out, "pre-sort");
+    upper->writeStats(*out, "post-sort");
+    ssdDevice->writeStats(*out);
+    hddDevice->writeStats(*out);
+
+    assert(("The count of the lower witness should have equaled the record count" && recordCount == lower->getCount()));
+    assert(("The count of the upper witness should equaled the record count" && recordCount  == upper->getCount()));
     assert(("The checksum of the lower witness did not equal the checksum of the upper but should have" && lower->getChecksum() == upper->getChecksum()));
     assert(("The lower was sorted but should not have been" && !lower->isSorted()));
     assert(("The upper witness was not sorted but should have been" && upper->isSorted()));
