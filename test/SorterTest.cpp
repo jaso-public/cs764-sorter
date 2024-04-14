51bb48b72661848a152aea6a39c39c7e2a299e44
morgan <mmnelson7@wisc.edu>
2024-04-13 19:52:15 -0500

    updated SorterTest.cpp

--- test/SorterTest.cpp
+++ test/SorterTest.cpp
@@ -13,7 +13,7 @@
 
 using namespace std;
 
-void testSpillToHdd() {
+void testSpill110ToHdd() {
     string test = "testSpillToHdd: ";
     Record::staticInitialize(100);
     int recordCount = 110;
@@ -28,8 +28,6 @@ void testSpillToHdd() {
     cfg->hddReadSize = 1000;
     cfg->ssdReadSize = 500;
     cfg->ssdStorageSize = 50000;
-
-
 
     auto source = make_shared<RandomProvider>(recordCount);
     auto lower = make_shared<Witness>(source);
@@ -58,16 +56,21 @@ void testSpillToHdd() {
     assert(("The upper witness was not sorted but should have been" && upper->isSorted()));
 }
 
+void testSorting(string testName, int recordSize, int recordCount){
+    Record::staticInitialize(recordSize);
 
-void testSmallSort() {
-    string testName = "testSmallSort";
-    int count = 20;
+    unique_ptr<SorterConfig> cfg = make_unique<SorterConfig>();
+    cfg->memoryBlockCount = 10;
+    cfg->memoryBlockSize = 1000;
+    auto ssdDevice = make_shared<IODevice>("ssd.stage");
+    auto hddDevice = make_shared<IODevice>("hdd.stage");
+    cfg->ssdDevice = ssdDevice;
+    cfg->hddDevice = hddDevice;
+    cfg->hddReadSize = 1000;
+    cfg->ssdReadSize = 500;
+    cfg->ssdStorageSize = 50000;
 
-    Record::staticInitialize(20);
-
-    auto cfg = make_unique<SorterConfig>();
-
-    auto provider = make_shared<RandomProvider>(count, false);
+    auto provider = make_shared<RandomProvider>(recordCount, false);
     auto printer1 = make_shared<Printer>(provider, testName+"-before");
     auto lower = make_shared<Witness>(printer1);
     auto sorter = make_shared<Sorter>(cfg, lower);
@@ -81,159 +84,15 @@ void testSmallSort() {
     std::ostream* out = &std::cout;
     device->writeStats(*out);
 
-    assert(("The count of the lower witness should have equaled the record count" && count == lower->getCount()));
-    assert(("The count of the upper witness should equaled the record count" && count == upper->getCount()));
+    assert(("The count of the lower witness should have equaled the record count" && recordCount == lower->getCount()));
+    assert(("The count of the upper witness should equaled the record count" && recordCount == upper->getCount()));
     assert(("The checksum of the lower witness did not equal the checksum of the upper but should have"
-        && lower->getChecksum() == upper->getChecksum()));
-    assert(("The lower was sorted but should not have been" && !lower->isSorted()));
+            && lower->getChecksum() == upper->getChecksum()));
+    if (recordCount != 0){
+        assert(("The lower was sorted but should not have been" && !lower->isSorted()));
+    }
     assert(("The upper witness was not sorted but should have been" && upper->isSorted()));
 }
-
-//
-//void testAllMemory() {
-//
-//    string test = "testAllMemory: ";
-//    SorterConfig* cfg = new SorterConfig();
-//    cfg->recordCount = 90000;
-//    cfg->recordSize = 1000;
-//    auto records = generateRandom(90000);
-//    ArrayProvider provider("name", records);
-//    Witness lower(&provider);
-//    SorterConfig* cfg2 = new SorterConfig();
-//    Sorter sorter(*cfg2, &lower);
-//    Witness upper(&sorter);
-//    Consumer consumer(&upper);
-//    consumer.consume();
-//
-//    sorter.printStats();
-//
-//    assert(("The count of the lower witness should have equaled the record count" && cfg->recordCount == lower.getCount()));
-//    assert(("The count of the upper witness should equaled the record count" && cfg->recordCount == upper.getCount()));
-//    assert(("The checksum of the lower witness did not equal the checksum of the upper but should have" && lower.getCrc() == upper.getCrc()));
-//    assert(("The lower was sorted but should not have been" && !lower.isSorted));
-//    assert(("The upper witness was not sorted but should have been" && upper.isSorted));
-//}
-//
-//void testSpillToSsdFewBlocks() {
-//    string test = "testSpillToSsdFewBlocks: ";
-//    SorterConfig* cfg = new SorterConfig();
-//    cfg->recordCount = 1024*100;
-//    cfg->recordSize = 1024;
-//    auto records = generateRandom( 1024*100);
-//    ArrayProvider provider("name", records);
-//    Witness lower(&provider);
-//    SorterConfig* cfg2 = new SorterConfig();
-//    Sorter sorter(*cfg2, &lower);
-//    Witness upper(&sorter);
-//    Consumer consumer(&upper);
-//    consumer.consume();
-//
-//    sorter.printStats();
-//
-//    assert(("The count of the lower witness should have equaled the record count" && cfg->recordCount == lower.getCount()));
-//    assert(("The count of the upper witness should equaled the record count" && cfg->recordCount == upper.getCount()));
-//    assert(("The checksum of the lower witness did not equal the checksum of the upper but should have" && lower.getCrc() == upper.getCrc()));
-//    assert(("The lower was sorted but should not have been" && !lower.isSorted));
-//    assert(("The upper witness was not sorted but should have been" && upper.isSorted));
-//}
-//
-//void testSpillToSsd() {
-//    string test = "testSpillToSsd: ";
-//    SorterConfig* cfg = new SorterConfig();
-//    cfg->recordCount = 900000;
-//    cfg->recordSize = 1000;
-//    auto records = generateRandom( 900000);
-//    ArrayProvider provider("name", records);
-//    Witness lower(&provider);
-//    SorterConfig* cfg2 = new SorterConfig();
-//    cfg2->recordCount = 900000;
-//    cfg2->recordSize = 1000;
-//    Sorter sorter(*cfg2, &lower);
-//    Witness upper(&sorter);
-//    Consumer consumer(&upper);
-//    consumer.consume();
-//
-//    sorter.printStats();
-//
-//    assert(("The count of the lower witness should have equaled the record count" && cfg->recordCount == lower.getCount()));
-//    assert(("The count of the upper witness should equaled the record count" && cfg->recordCount == upper.getCount()));
-//    assert(("The checksum of the lower witness did not equal the checksum of the upper but should have" && lower.getCrc() == upper.getCrc()));
-//    assert(("The lower was sorted but should not have been" && !lower.isSorted));
-//    assert(("The upper witness was not sorted but should have been" && upper.isSorted));
-//}
-//
-//void testSpillToHdd() {
-//    string test = "testSpillToLotsOfHddRuns: ";
-//    SorterConfig* cfg = new SorterConfig();
-//    cfg->recordCount = 190000;
-//    cfg->recordSize = 1000;
-//    auto records = generateRandom( 190000);
-//    ArrayProvider provider("name", records);
-//    Witness lower(&provider);
-//    SorterConfig* cfg2 = new SorterConfig();
-//    Sorter sorter(*cfg2, &lower);
-//    Witness upper(&sorter);
-//    Consumer consumer(&upper);
-//    consumer.consume();
-//
-//    sorter.printStats();
-//
-//    assert(("The count of the lower witness should have equaled the record count" && cfg->recordCount == lower.getCount()));
-//    assert(("The count of the upper witness should equaled the record count" && cfg->recordCount == upper.getCount()));
-//    assert(("The checksum of the lower witness did not equal the checksum of the upper but should have" && lower.getCrc() == upper.getCrc()));
-//    assert(("The lower was sorted but should not have been" && !lower.isSorted));
-//    assert(("The upper witness was not sorted but should have been" && upper.isSorted));
-//}
-//
-//void testSpillToLotsOfHddRuns() {
-//    string test = "testSpillToLotsOfHddRuns: ";
-//    SorterConfig* cfg = new SorterConfig();
-//    cfg->recordCount = 190000;
-//    cfg->recordSize = 1000;
-//    cfg->memoryBlockCount = 10;
-//    cfg->memoryBlockSize = 100*1024*1024;
-//    auto records = generateRandom( 190000);
-//    ArrayProvider provider("name", records);
-//    Witness lower(&provider);
-//    SorterConfig* cfg2 = new SorterConfig();
-//    Sorter sorter(*cfg2, &lower);
-//    Witness upper(&sorter);
-//    Consumer consumer(&upper);
-//    consumer.consume();
-//
-//    sorter.printStats();
-//
-//    assert(("The count of the lower witness should have equaled the record count" && cfg->recordCount == lower.getCount()));
-//    assert(("The count of the upper witness should equaled the record count" && cfg->recordCount == upper.getCount()));
-//    assert(("The checksum of the lower witness did not equal the checksum of the upper but should have" && lower.getCrc() == upper.getCrc()));
-//    assert(("The lower was sorted but should not have been" && !lower.isSorted));
-//    assert(("The upper witness was not sorted but should have been" && upper.isSorted));
-//}
-//
-//void testZeroRecords() {
-//    string test = "testZeroRecords: ";
-//    SorterConfig* cfg = new SorterConfig();
-//    cfg->recordCount = 0;
-//    cfg->recordSize = 1000;
-//    auto records = generateRandom( 0);
-//    ArrayProvider provider("name", records);
-//    Witness lower(&provider);
-//    SorterConfig* cfg2 = new SorterConfig();
-//    Sorter sorter(*cfg2, &lower);
-//    Witness upper(&sorter);
-//    Consumer consumer(&upper);
-//    consumer.consume();
-//
-//    sorter.printStats();
-//
-//    assert(("The count of the lower witness should have equaled 0" && 0 == lower.getCount()));
-//    assert(("The count of the upper witness should equaled 0" && 0 == upper.getCount()));
-//    assert(("The count of the lower witness should have equaled the record count" && cfg->recordCount == lower.getCount()));
-//    assert(("The count of the upper witness should equaled the record count" && cfg->recordCount == upper.getCount()));
-//    assert(("The checksum of the lower witness did not equal the checksum of the upper but should have" && lower.getCrc() == upper.getCrc()));
-//    assert(("The lower was sorted but should not have been" && !lower.isSorted));
-//    assert(("The upper witness was not sorted but should have been" && upper.isSorted));
-//}
 
 
 void testSorterConfigInitialization() {
@@ -250,13 +109,13 @@ void testSorterConfigInitialization() {
 
 
 int main() {
-    testSpillToHdd();
-//    testSorterConfigInitialization();
-//    testSmallSort();
-//    testAllMemory();
-//    testSpillToSsdFewBlocks();
-//    testSpillToSsd();
-//    testSpillToHdd();
-//    testSpillToLotsOfHddRuns();
-//    testZeroRecords();
+    testSpill110ToHdd();
+    testSorting("testSmallSort", 20, 20);
+    //TODO: testAllMemory fails plus others below
+    testSorting("testAllMemory: ", 1000, 9000);
+    testSorting("testSpillToSsdFewBlocks: ", 1024, 1024*100);
+    testSorting("testSpillToSsd: ", 1000, 900000);
+    testSorting("testSpillToLotsOfHddRuns: ", 1000, 190000);
+    testSorting("testZeroRecords: ", 1000, 0);
+    testSorterConfigInitialization();
 }
