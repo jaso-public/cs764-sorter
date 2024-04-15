27a315aeb93bb45792568e362c67e7b93e5401d6
morgan <mmnelson7@wisc.edu>
2024-04-15 14:51:36 -0500

    seperate sort and generate

--- Testing/TestGenerateRecords.cpp
+++ Testing/TestGenerateRecords.cpp
@@ -1,7 +1,10 @@
-#include "./src/tools/GenerateMain.cpp"
-#include "./src/tools/SortMain.cpp"
+#include "./src/tools/Generate.h"
+#include "./src/tools/SortMain.h"
 
-    void testGenerateAndSortRecords(int recordSize, int recordCount, double probability, int range) {
+#include <string>
+using namespace std;
+
+void testGenerateAndSortRecords(int recordSize, int recordCount, double probability, int range) {
         string inFile = "test-input.txt";
         const char* generateArgs[] = {"generate", "-s", nullptr, "-c", nullptr, "-f", nullptr, "-p", nullptr, "-r", nullptr};
         generateArgs[2] = to_string(recordSize).c_str();
@@ -11,25 +14,33 @@
         generateArgs[10] = to_string(range).c_str();
         int generateArgsCount = sizeof(generateArgs) / sizeof(generateArgs[0]);
         generateMain(generateArgsCount, const_cast<char**>(generateArgs));
+}
 
-        string outFile = "test-output.txt";
-        string ssdStagingFile = "test-ssd-staging.txt";
-        string hddStagingFile = "test-hdd-staging.txt";
-        const char* sortArgs[] = {"sort", "-s", nullptr, "-i", nullptr, "-j", nullptr, "-d", nullptr, "-h", nullptr};
-        sortArgs[2] = to_string(recordSize).c_str();
-        sortArgs[4] = inFile.c_str();
-        sortArgs[6] = outFile.c_str();
-        sortArgs[8] = ssdStagingFile.c_str();
-        sortArgs[10] = hddStagingFile.c_str();
-        int sortArgsCount = sizeof(sortArgs) / sizeof(sortArgs[0]);
-        sortMain(sortArgsCount,const_cast<char**>(sortArgs));
+void testSort(int recordSize){
+//    string inFile = "test-input.txt";
+//    string outFile = "test-output.txt";
+//    string ssdStagingFile = "test-ssd-staging.txt";
+//    string hddStagingFile = "test-hdd-staging.txt";
+    const char* sortArgs[] = {"sort", "-s", nullptr
+//                              ,"-i", nullptr, "-j", nullptr, "-d", nullptr, "-h", nullptr
+    };
+    sortArgs[2] = to_string(recordSize).c_str();
+//    sortArgs[4] = inFile.c_str();
+//    sortArgs[6] = outFile.c_str();
+//    sortArgs[8] = ssdStagingFile.c_str();
+//    sortArgs[10] = hddStagingFile.c_str();
+    int sortArgsCount = sizeof(sortArgs) / sizeof(sortArgs[0]);
+    sortMain(sortArgsCount,const_cast<char**>(sortArgs));
 
-        remove(inFile.c_str());
-        remove(outFile.c_str());
-        remove(ssdStagingFile.c_str());
-        remove(hddStagingFile.c_str());
-    }
+//    remove(inFile.c_str());
+//    remove(outFile.c_str());
+//    remove(ssdStagingFile.c_str());
+//    remove(hddStagingFile.c_str());
+
+}
 
 int main(){
+    testSort(20);
     testGenerateAndSortRecords(20, 100, 0.01, 999);
+
 }
