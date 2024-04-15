55875713f305dff403a615e0f406c47fbdeffbc6
jaso Sorenson <jaso@cs.wisc.edu>
2024-04-15 13:43:39 -0500

    a little sorter cleanup and silenced some test noise.

--- src/Sorter.cpp
+++ src/Sorter.cpp
@@ -75,6 +75,12 @@ shared_ptr<Provider> Sorter::startSort() {
 
         if (recordCount > 0) {
             // find the buffer that we are going to fill.
+            // the first memory block of the buffer is used by the storeRun
+            // method to stage data before writing to ssd or hdd.
+            // lastMemoryRun is the offset of the last memory run stored.
+            // so if we have enough free space, then the current memory run
+            // will be stored at lastMemoryRun - the size of a memoryRun
+            // (which is the size of a cache block)
             if (lastMemoryRun == cfg->memoryBlockSize) makeFreeSpace();
 
             lastMemoryRun -= cfg->memoryBlockSize;
@@ -99,10 +105,13 @@ shared_ptr<Provider> Sorter::startSort() {
     int memoryRequired = ssdRuns.size() * cfg->ssdReadSize + (hddRuns.size() + 1) * cfg->hddReadSize;
     if (memoryRequired < cfg->memoryBlockCount * cfg->memoryBlockSize) {
         if (memoryRequired > lastMemoryRun) {
-            int toRelease = (memoryRequired - lastMemoryRun + cfg->memoryBlockSize - 1) / cfg->memoryBlockSize;
-            releaseMemory(toRelease);
+            int memoryToRelease = roundUp(memoryRequired - lastMemoryRun, cfg->memoryBlockSize);
+            int runsToRelease = memoryToRelease / cfg->memoryBlockSize;
+            releaseMemory(runsToRelease);
         }
 
+        // set up the providers for the final merge run.
+        // add all the memory to the provider as well as ssd/hdd runs with the device's perferred io size.
         vector<shared_ptr<Provider>> providers(memoryRuns.size() + ssdRuns.size() + hddRuns.size());
         int index = 0;
         for (shared_ptr<Run> run: memoryRuns) {
@@ -139,8 +148,8 @@ shared_ptr<Provider> Sorter::startSort() {
         return result;
     }
 
-      // we did not have enough memory to do the final merge, so lets flush all of our memory
-      // runs and use all the memory to stage the final merge run.
+    // we did not have enough memory to do the final merge, so lets flush all of our memory
+    // runs and use all the memory to stage the final merge run.
     releaseMemory(memoryRuns.size());
 
 
@@ -148,9 +157,9 @@ shared_ptr<Provider> Sorter::startSort() {
     // 1. do we have enough free space on the ssd to do the staging?
     // 2. do we have too many runs to merge in the final pass?
 
-
     // max number of merge runs that can be merged using cfg.ssdReadSize chunk sized memory for each run
     // Note we reserve one cfg.hddReadSize block of memory to transfer data from the hdd back to the ssd
+    // and each run requires enough space to read an ssd read sized chunk into memory
     int maxMergeRuns = ((cfg->memoryBlockCount * cfg->memoryBlockSize) - cfg->hddReadSize) / cfg->ssdReadSize;
     int runsToMergeForCount = ssdRuns.size() + hddRuns.size() - maxMergeRuns;
 
@@ -165,6 +174,7 @@ shared_ptr<Provider> Sorter::startSort() {
     int runsToMerge = max(runsToMergeForSpace, runsToMergeForCount);
 
     cout << "------------------" << "\n";
+    cout << "maxMergeRuns:" << maxMergeRuns << endl;
     cout << "memoryRuns.size():" << memoryRuns.size() << "\n";
     cout << "ssdRuns.size():" << ssdRuns.size() << "\n";
     cout << "hddRuns.size():" << hddRuns.size() << "\n";
@@ -177,59 +187,59 @@ shared_ptr<Provider> Sorter::startSort() {
     vector<shared_ptr<Provider>> providers(runsToMerge);
 
     long recordCount = 0;
+    int offset = cfg->memoryBlockSize; // leave the first block of memory to stage data when storing the run
     for (int i = 0; i < runsToMerge; i++) {
         shared_ptr<Run> run = ssdRuns[0];
         ssdRuns.erase(ssdRuns.begin());
         recordCount += run->numRecords;
-        int offset = cfg->memoryBlockSize + i * cfg->ssdReadSize;
 
         unique_ptr<StorageConfig> storageConfig = make_unique<StorageConfig>();
         storageConfig->recordCount = run->numRecords;
         storageConfig->startOffset = run->offset;
-        storageConfig->buffer = buffer;
+        storageConfig->buffer = buffer + offset;
         storageConfig->bufferLength = cfg->ssdReadSize;
         storageConfig->storage = cfg->ssdDevice;
         providers[i] = make_shared<StorageProvider>(storageConfig);
+
+        offset += cfg-> ssdReadSize;  // advance the offset so each provider gets its own space
     }
 
     storeRun(make_shared<TournamentPQ>(providers, runsToMerge), recordCount);
 
-    vector<shared_ptr<Provider>> newProvider(ssdRuns.size() + hddRuns.size());
-    providers = newProvider;
-    int index = 0;
+    providers.clear();
     int memoryOffset = cfg->hddReadSize;
+    uint64_t stagingOffset = 0;
 
-
-    for (int i = 0; i < hddRuns.size(); i++) {
-        shared_ptr<Run> run = hddRuns[i];
-
+    for(shared_ptr<Run> run : hddRuns) {
         unique_ptr<StagingConfig> stagingCfg = make_unique<StagingConfig>();
         stagingCfg->recordCount = run->numRecords;
         stagingCfg->storage = cfg->hddDevice;
         stagingCfg->storageStartOffset = run->offset;
         stagingCfg->staging = cfg->ssdDevice;
-        stagingCfg->stagingStartOffset = i * (cfg->hddReadSize - cfg->ssdReadSize);
+        stagingCfg->stagingStartOffset = stagingOffset;
         stagingCfg->stagingLength = cfg->hddReadSize - cfg->ssdReadSize;
         stagingCfg->buffer = buffer + memoryOffset;
         stagingCfg->bufferLength = cfg->ssdReadSize;
         stagingCfg->transferBuffer = buffer; // this is always at the start of the memory buffer
         stagingCfg->transferLength = cfg->hddReadSize;
-        providers[index++] = make_shared<StagedProvider>(stagingCfg);
+        providers.push_back(make_shared<StagedProvider>(stagingCfg));
+
         memoryOffset += cfg->ssdReadSize;
+        stagingOffset += (cfg->hddReadSize - cfg->ssdReadSize);
     }
 
-    for (shared_ptr<Run> run: ssdRuns) {
+    for(shared_ptr<Run> run : ssdRuns) {
         unique_ptr<StorageConfig> storageConfig = make_unique<StorageConfig>();
         storageConfig->recordCount = run->numRecords;
         storageConfig->startOffset = run->offset;
         storageConfig->buffer = buffer;
         storageConfig->bufferLength = cfg->ssdReadSize;
         storageConfig->storage = cfg->ssdDevice;
-        providers[index++] = make_shared<StorageProvider>(storageConfig);
+        providers.push_back(make_shared<StorageProvider>(storageConfig));
         memoryOffset += cfg->ssdReadSize;
     }
 
-    shared_ptr<Provider> result = make_shared<TournamentPQ>(providers, index);
+    shared_ptr<Provider> result = make_shared<TournamentPQ>(providers, providers.size());
     return result;
 }
 
